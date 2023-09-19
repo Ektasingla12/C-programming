61377545f54dc32a130c7e43480d4966bc6b14d1
Ekta <122769810+Ektasingla12@users.noreply.github.com>
2023-09-19 18:21:53 +0530

    sum of two numbers

---
+++ basic/sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter value of a");
+    scanf("%d",&a);
+    printf("enter value of b");
+    scanf("%d",&b);
+    c=a+b;      
+    printf(" sum of a and b is %d + %d =%d",a,b,c);
+	return 0;
+}
