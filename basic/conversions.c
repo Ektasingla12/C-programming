db779e1360ba59a8043bff0767eb54cc29952a6e
Ekta <122769810+Ektasingla12@users.noreply.github.com>
2023-09-20 20:34:32 +0530

    conversions m,cm,f,i

---
+++ basic/conversions.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,m,cm,f,i;
+	scanf("%d",&a);
+	m=a*1000;
+	cm=a*100000;
+	f=3980.84*a;
+	i=a*39370.08;o9,
+	printf("meter centimeter feet inches %d %d %d %d",m,cm,f,i);
+	return 0;
+}
