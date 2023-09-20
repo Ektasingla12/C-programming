28dcb35abe0ea2a01dae7acc1a552a60c3a89f1f
Ekta <122769810+Ektasingla12@users.noreply.github.com>
2023-09-20 20:44:00 +0530

    even and odd number

---
+++ loops/even odd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter value of a");
+	scanf("%d",&a);
+	if(a%2==0)
+	printf("a is even number");
+	else
+	printf("a is odd number");
+	return 0;
+}
