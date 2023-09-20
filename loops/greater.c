7c4c2813aa647439623fea846193e6dcbfbf66ef
Ekta <122769810+Ektasingla12@users.noreply.github.com>
2023-09-20 20:39:59 +0530

    greater a and b

---
+++ loops/greater.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter value of a and b");
+	scanf("%d %d",&a,&b);
+	if(a>b)
+	printf("%d is greater",a);
+	else 
+	printf("%d is greater",b);
+	
+	return 0;
+}
