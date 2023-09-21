140ce4fd151fa820d461154b0712816569817992
Ekta <122769810+Ektasingla12@users.noreply.github.com>
2023-09-21 17:22:46 +0530

    natural number from 1 to n

---
+++ loops/natural number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,n;
+    printf("enter value of n number");
+    scanf("%d",&n);
+    a=1;
+    while(a<=n)
+    {
+        printf("%d\n",a);
+        a++;
+    }
+    return 0;
+}
