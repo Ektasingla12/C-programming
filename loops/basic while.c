024e7771d1aee1a1e1444611f6f9a3f6e763c008
Ekta <122769810+Ektasingla12@users.noreply.github.com>
2023-09-21 16:53:44 +0530

    basic while

---
+++ loops/basic while.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    a=0;                    // a is initial value
+    while(a<10)             //loop work when a<10
+    {
+        printf("%d",a);
+        a++;                //increment
+    
+    }
+    return 0;
+}
