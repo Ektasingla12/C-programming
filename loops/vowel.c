8d717c18a708785b3b9a675e6014e9029fb810bb
Ekta <122769810+Ektasingla12@users.noreply.github.com>
2023-09-21 16:43:50 +0530

    vowel

---
+++ loops/vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	char z;
+	scanf("%c",&z);
+	if(z=='a' || z=='e' || z=='i' || z=='o' ||z=='u')
+	{
+	printf("vowels");
+    }
+	else 
+	{
+	printf("not vowels");
+    }
+    return 0;
+}
