0df487179a8368ec074cfa0b79e6b2e2fb1dac3e
Ekta <122769810+Ektasingla12@users.noreply.github.com>
2023-09-26 18:44:11 +0530

    maximum number

---
+++ array/2D/maximum.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,m,j;
+    printf("enter length of array n and m ");
+    scanf("%d %d",&n,&m);
+    int arr[n][m];
+    printf("enter value of array ");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            printf("%d\t",arr[i][j]);
+        }
+        printf("\n");
+    }
+    int max=0;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            if(max<arr[i][j])
+            {
+            max=arr[i][j];
+			}
+        }
+    }
+    printf("maximum number is %d ",max);
+    return 0;
+}
