5c7336e5821b9737be885820b3c6172261d74285
Ekta <122769810+Ektasingla12@users.noreply.github.com>
2023-09-26 17:54:20 +0530

    basic 2D array

---
+++ array/2D/basic.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,m,j;
+    printf("enter length of array n and m ");
+    scanf("%d %d",&n,&m);
+    int arr[n][m];
+    printf("enter value of array ");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%d\t",arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
