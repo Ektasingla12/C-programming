c183b44cfa30ef2e367e99de4b1b2ebbb2aec263
Ekta <122769810+Ektasingla12@users.noreply.github.com>
2023-10-27 18:41:07 +0530

    Alphabet E

---
+++ pattern/alphabet E.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+	{
+			printf("*");
+	}
+	for(i=0;i<=n;i++)
+	{
+		for(j=0;j<=n;j++)
+		{
+			printf("*");
+			if(j=i+2)
+			break;
+		}
+		printf("\n");
+	}
+	for(i=0;i<=n;i++)
+	{
+		printf("*");
+	}
+	for(i=0;i<=n;i++)
+	{
+		for(j=0;j<=n;j++)
+		{
+			if(j==n)
+				printf("*");
+		}
+		printf("\n");
+	}
+		for(i=0;i<=n;i++)
+	{
+		printf("*");
+	}
+	return 0;
+}
+
+
+
+
+input:3
+output:
+*****
+*
+*
+*
+*****
+*
+*
+*
+****
