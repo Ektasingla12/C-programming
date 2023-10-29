bd6411a454066db9a0c7a182fc2613ff6a1b7791
Ekta <122769810+Ektasingla12@users.noreply.github.com>
2023-10-29 18:29:08 +0530

    get and put char

---
+++ strings/get and put char.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	
+	char a;
+	printf("enter first alphabet of ur name:");
+	a=getchar();     //getchar helps as scanf or taking output
+	printf("ur name starts with :%c ",a);
+	char cha='g';
+	printf("\nvalue of putchar : ");
+	putchar(cha);   //putchar helps to print the value 
+	return 0;
+}
